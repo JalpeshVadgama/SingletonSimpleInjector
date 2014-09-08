2e1082543525b0181e53278be00bd6ca30b46970
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2014-09-09 01:10:33 +0530

    Initial commit for console application

---
+++ SimpleInjectorSingleTonExample/SimpleInjectorSingleTonExample/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleInjectorSingleTonExample
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
---
+++ SimpleInjectorSingleTonExample/SimpleInjectorSingleTonExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using SimpleInjector;
+
+namespace SimpleInjectorSingleTonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new Container();
+            var registartion = Lifestyle.Singleton.CreateRegistration<ILogger,Logger>(container);
+            container.AddRegistration(typeof(ILogger),registartion);
+            var logger = container.GetInstance<ILogger>();
+            logger.Log("This is a sample Log");
+        }
+    }
+}
