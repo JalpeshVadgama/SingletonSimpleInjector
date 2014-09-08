ef40e903ace8f8b0cb0a74100a11dc5831812429
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2014-09-09 01:11:49 +0530

    Another method for singleton instance

--- SimpleInjectorSingleTonExample/SimpleInjectorSingleTonExample/Program.cs
+++ SimpleInjectorSingleTonExample/SimpleInjectorSingleTonExample/Program.cs
@@ -7,6 +7,7 @@ namespace SimpleInjectorSingleTonExample
         static void Main(string[] args)
         {
             var container = new Container();
+            //container.RegisterSingle<ILogger, Logger>();
             var registartion = Lifestyle.Singleton.CreateRegistration<ILogger,Logger>(container);
             container.AddRegistration(typeof(ILogger),registartion);
             var logger = container.GetInstance<ILogger>();
